b2bce4fc6a399c710dfdfe04f7fc40ea5a426e9e
Titus Buchanan <51974714+TitusBuchanan@users.noreply.github.com>
2025-09-16 12:39:00 -0400

    TD-6 changed clock functionality

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
-      hour12: true
+      hour12: false
     }).format(now);
 
     const date = new Intl.DateTimeFormat(undefined, {
